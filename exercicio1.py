ef76c22fb28380d9d77881fb059bd23355056d9b
Fernanda Raquel <santos.mendes@escolar.ifrn.edu.br>
2025-09-19 14:27:18 -0300

    teste

---
+++ exercicio1.py
@@ -0,0 +1,28 @@
+#Questão 01 - Projeto Euler - Problema 34
+#Projeto Euler - Problema 34
+#145é um número curioso, pois1!+4!+5!=1+24+120=145.
+#Encontre a soma de todos os números que são iguais à soma do fatorial de seus dígitos.
+#Nota: Como1!=1e2!=2não são somas, elas não estão incluídas.
+
+
+for i in range (10,2540161)
+if i == 1 or 1==2: continue    #caso queira começar com 1 pq no problema diz que esse nao vai entrar
+
+
+
+#gabarito = 145 1° numero encontrado
+#40585 = 2° numero encontrado
+#40730(soma dos dois numeros encontrados)
+
+#n = fatorial (digito)
+
+#9! = 362880
+#1 x 9! = 362.880
+#2 x 9! = 725.760
+#3 x 9! = 1.088.640
+#4 x 9! = 1.451.520
+#5 x 9! = 1.814.400
+#6 x 9! = 2.177.280
+#7 x 9! = 2.540.160
+#8 x 9! = 2.903.040
+#9 x 9! = 3.265.920
